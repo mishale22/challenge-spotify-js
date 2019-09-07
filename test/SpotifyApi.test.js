a2252d444f03c85fca0854804c1e5504b42051ce
Sandra Niño <sami_na11@hotmail.com>
2019-09-07 11:52:33 -0500

    Song search (#3)
    * Song search
    
    * adjustment

--- test/SpotifyApi.test.js
+++ test/SpotifyApi.test.js
@@ -4,7 +4,10 @@ const chai = require('chai');
 
 const { expect } = chai;
 
-const urlBase = 'https://accounts.spotify.com';
+const accountUrl = 'https://accounts.spotify.com';
+const apiUrl = 'https://api.spotify.com';
+
+let token = '';
 
 describe('Spotify Api Testing', () => {
   describe('Authentication', () => {
@@ -15,11 +18,35 @@ describe('Spotify Api Testing', () => {
 
       const header = `Basic ${process.env.CLIENT_ID_CLIENT_SECRET}`;
 
-      const response = await agent.post(`${urlBase}/api/token`).set('Content-Type', 'application/x-www-form-urlencoded').set('Authorization', header).send(requestBody);
+      const response = await agent.post(`${accountUrl}/api/token`)
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .set('Authorization', header)
+        .send(requestBody);
+
+      token = response.body.access_token;
 
       expect(response.status).to.equal(statusCode.OK);
       expect(response.body.token_type).to.equal('Bearer');
       expect(response.body).to.have.property('access_token');
+    });
+  });
+
+  describe('Spotify search', () => {
+    it('Song search', async () => {
+      const query = {
+        q: 'Till I Collapse',
+        type: 'track',
+        limit: 20
+      };
+
+      const authHeader = `Bearer ${token}`;
+
+      const response = await agent.get(`${apiUrl}/v1/search`)
+        .set('Authorization', authHeader)
+        .query(query);
+
+      expect(response.status).to.equal(statusCode.OK);
+      expect(response.body.tracks.items).lengthOf(20);
     });
   });
 });
