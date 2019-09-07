69ae7b0aa9545ec6243dbb416fed308456fbe8e3
mishale22 <sami_na11@hotmail.com>
2019-09-06 20:32:53 -0500

    Song search

--- test/SpotifyApi.test.js
+++ test/SpotifyApi.test.js
@@ -5,6 +5,9 @@ const chai = require('chai');
 const { expect } = chai;
 
 const urlBase = 'https://accounts.spotify.com';
+const urlBase2 = 'https://api.spotify.com';
+
+let token = '';
 
 describe('Spotify Api Testing', () => {
   describe('Authentication', () => {
@@ -17,9 +20,28 @@ describe('Spotify Api Testing', () => {
 
       const response = await agent.post(`${urlBase}/api/token`).set('Content-Type', 'application/x-www-form-urlencoded').set('Authorization', header).send(requestBody);
 
+      token = response.body.access_token;
+
       expect(response.status).to.equal(statusCode.OK);
       expect(response.body.token_type).to.equal('Bearer');
       expect(response.body).to.have.property('access_token');
+    });
+  });
+
+  describe('Spotify search', () => {
+    it('Song search', async () => {
+      const query = {
+        q: 'Till I Collapse',
+        type: 'track',
+        limit: 20
+      };
+
+      const header = `Bearer ${token}`;
+
+      const response = await agent.get(`${urlBase2}/v1/search`).set('Authorization', header).query(query);
+
+      expect(response.status).to.equal(statusCode.OK);
+      expect(response.body.tracks.items).lengthOf(20);
     });
   });
 });
