2998381ff40aa64bb683506d123f41bea97b4553
Sandra Niño <sami_na11@hotmail.com>
2019-09-06 19:24:20 -0500

    Spotify authorization (#2)
    * Spotify authorization
    
    * Spotify authorization without hardcoding credentials :)
    
    * travis encrypt
    
    * EOF

--- test/SpotifyApi.test.js
+++ test/SpotifyApi.test.js
@@ -3,3 +3,23 @@ const statusCode = require('http-status-codes');
 const chai = require('chai');
 
 const { expect } = chai;
+
+const urlBase = 'https://accounts.spotify.com';
+
+describe('Spotify Api Testing', () => {
+  describe('Authentication', () => {
+    it('Auth Spotify', async () => {
+      const requestBody = {
+        grant_type: 'client_credentials'
+      };
+
+      const header = `Basic ${process.env.CLIENT_ID_CLIENT_SECRET}`;
+
+      const response = await agent.post(`${urlBase}/api/token`).set('Content-Type', 'application/x-www-form-urlencoded').set('Authorization', header).send(requestBody);
+
+      expect(response.status).to.equal(statusCode.OK);
+      expect(response.body.token_type).to.equal('Bearer');
+      expect(response.body).to.have.property('access_token');
+    });
+  });
+});
